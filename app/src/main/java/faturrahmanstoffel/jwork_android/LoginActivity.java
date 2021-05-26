df43dc9716c2cc45f4e656c7c4306414515b1360
fatur <faturrahmanstoffel90@gmail.com>
2021-05-26 18:26:39 +0700

    Modul 9 - TP

---
+++ app/src/main/java/faturrahmanstoffel/jwork_android/LoginActivity.java
@@ -0,0 +1,63 @@
+package faturrahmanstoffel.jwork_android;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText fieldEmail = findViewById(R.id.fieldEmail);
+        EditText fieldPassword = findViewById(R.id.fieldPassword);
+        Button tombolLogin = findViewById(R.id.tombolLogin);
+        TextView textRegister = findViewById(R.id.textRegister);
+
+        tombolLogin.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view){
+             String email = fieldEmail.getText().toString();
+             String password = fieldPassword.getText().toString();
+             Response.Listener<String> responseListener = new Response.Listener<String>() {
+                 @Override
+                 public void onResponse(String response) {
+                     try {
+                         JSONObject jsonObject = new JSONObject(response);
+                         if(jsonObject != null){
+                             Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                         }
+                     } catch (JSONException e) {
+                         Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                     }
+                 }
+             };
+
+             LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+             RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+             queue.add(loginRequest);
+         }
+        });
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/faturrahmanstoffel/jwork_android/RegisterActivity.java
@@ -0,0 +1,58 @@
+package faturrahmanstoffel.jwork_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText fieldNameRegister = findViewById(R.id.fieldNameRegister);;
+        EditText fieldEmailRegister = findViewById(R.id.fieldEmailRegister);
+        EditText fieldPasswordRegister = findViewById(R.id.fieldPasswordRegister);
+        Button tombolRegister = findViewById(R.id.tombolRegister);
+
+        tombolRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = fieldNameRegister.getText().toString();
+                String email = fieldEmailRegister.getText().toString();
+                String password = fieldPasswordRegister.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject != null){
+                                Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(name, email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
+            }
+        });
+    }
+}

8f12b18c8f2a27a6b14682b9be49686085b18931
fatur <faturrahmanstoffel90@gmail.com>
2021-05-27 18:03:24 +0700

    Modul 9 - CS

--- app/src/main/java/faturrahmanstoffel/jwork_android/LoginActivity.java
+++ app/src/main/java/faturrahmanstoffel/jwork_android/LoginActivity.java
@@ -40,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
                          JSONObject jsonObject = new JSONObject(response);
                          if(jsonObject != null){
                              Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                             startActivity(intent);
+                             finish();
                          }
                      } catch (JSONException e) {
                          Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
